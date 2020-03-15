a51f20ea02660ffd5a4e263d42788f4af89086a9
Rafal Pych <r.pych27@gmail.com>
2020-03-15 10:44:15 +0100

    init

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let liczba1 = 12;
+let notBigger = "podana liczba nie jest większa od 10";
+if (liczba1 > 10) {
+  console.log("Podana liczba jest większa od 10");
+} else {
+  console.log(notBigger);
+}
